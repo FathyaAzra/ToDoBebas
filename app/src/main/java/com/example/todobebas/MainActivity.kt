9f9eebfdba705ef6d9eda2d965fb45c94edf0ca7
Fathya Azra Nazmira 039 <108808542+FathyaAzra@users.noreply.github.com>
2024-09-19 08:01:38 +0700

    intent belum bisa

--- app/src/main/java/com/example/todobebas/MainActivity.kt
+++ app/src/main/java/com/example/todobebas/MainActivity.kt
@@ -1,20 +1,27 @@
 package com.example.todobebas
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button // Make sure to import Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.content.ContextCompat.startActivity
 
+fun onClick(view: View){
+    when (view.id){
+        R.id.btnmasuk -> {
+            val intent  = Intent(this, TodoActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
 class MainActivity : AppCompatActivity() {
+    private lateinit var button: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        enableEdgeToEdge() // Enable edge-to-edge for immersive experience
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
