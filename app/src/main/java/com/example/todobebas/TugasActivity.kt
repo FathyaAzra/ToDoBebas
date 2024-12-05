3f2c2e101177b8cfce9c3cf78c7e22739fe3199a
zihan-syakilla-daspro2 <zihansykl05@gmail.com>
2024-12-05 21:37:11 +0700

    zihan update dan perbaiki tombol btn mulai di main activity

---
+++ app/src/main/java/com/example/todobebas/TugasActivity.kt
@@ -0,0 +1,33 @@
+package com.example.todobebas
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.todobebas.ui.tugas.AddTask
+
+class TugasActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_tugas)
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+    }
+
+    private fun onClick(view: View) {
+        when (view.id) {
+            R.id.btnTambah -> {
+                val intent = Intent(this, AddTask::class.java)
+                startActivity(intent)
+            }
+
+        }
+    }
+}
