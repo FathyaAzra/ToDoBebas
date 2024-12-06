dc8555d626bbdaa1ac67a5a90c752819290f76c1
Khaisa-Shafa <khaisash09@gmail.com>
2024-12-06 10:27:43 +0700

    update

--- app/src/main/java/com/example/todobebas/MainActivity.kt
+++ app/src/main/java/com/example/todobebas/MainActivity.kt
@@ -6,7 +6,9 @@ import android.view.View
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.example.todobebas.ui.kalender.KalenderViewModel
 import com.example.todobebas.ui.tugas.TugasFragment
+import com.example.todobebas.ui.tugas.TugasViewModel
 import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
@@ -21,15 +23,25 @@ class MainActivity : AppCompatActivity() {
         button.setOnClickListener { onClick(it) }
 
     }
+
     private fun onClick(view: View) {
         when (view.id) {
             R.id.btnmasuk -> {
-                // Gunakan FragmentTransaction untuk memuat fragment
-                supportFragmentManager.beginTransaction()
-                    .replace(R.id.fragment_container, TugasFragment()) // Pastikan ID sesuai dengan layout container Anda
-                    .addToBackStack(null) // Tambahkan ke back stack (opsional)
-                    .commit()
+                val intent = Intent(this, TugasActivity::class.java)
+                startActivity(intent)
             }
+
         }
     }
+//    private fun onClick(view: View) {
+//        when (view.id) {
+//            R.id.btnmasuk -> {
+//                // Gunakan FragmentTransaction untuk memuat fragment
+//                supportFragmentManager.beginTransaction()
+//                    .replace(R.id.fragment_container, TugasFragment()) // Pastikan ID sesuai dengan layout container Anda
+//                    .addToBackStack(null) // Tambahkan ke back stack (opsional)
+//                    .commit()
+//            }
+//        }
+//    }
 }
