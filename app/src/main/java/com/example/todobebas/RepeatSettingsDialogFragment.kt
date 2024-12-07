aa3a36648ceb6c4138d83a8eb418fe0572c9dc2b
Khaisa-Shafa <khaisash09@gmail.com>
2024-12-07 22:35:45 +0700

    k

--- app/src/main/java/com/example/todobebas/RepeatSettingsDialogFragment.kt
+++ app/src/main/java/com/example/todobebas/RepeatSettingsDialogFragment.kt
@@ -6,8 +6,12 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.Switch
+import androidx.compose.ui.graphics.Color
 import androidx.fragment.app.DialogFragment
 import com.example.todobebas.R
+import android.content.Context;
+import androidx.core.content.ContextCompat;
+
 
 class RepeatSettingsDialogFragment : DialogFragment() {
 
@@ -18,12 +22,22 @@ class RepeatSettingsDialogFragment : DialogFragment() {
         val view = inflater.inflate(R.layout.dialog_repeat, container, false)
 
         val switchRepeat: Switch = view.findViewById(R.id.switch_repeat)
+        val btnDay : Button = view.findViewById(R.id.btn_day)
+        val btnWeek : Button = view.findViewById(R.id.btn_week)
+        val btnMonth : Button = view.findViewById(R.id.btn_month)
+        val btnYear : Button = view.findViewById(R.id.btn_year)
+
+//        val int colorOn = ContextCompat.getColor(this, R.color.lightblue)
+//        val int colorOff = ContextCompat.getColor(this, R.color.grey)
+
         val btnCancel: Button = view.findViewById(R.id.btn_cancel)
         val btnDone: Button = view.findViewById(R.id.btn_done)
 
-        // Toggle repeat options
         switchRepeat.setOnCheckedChangeListener { _, isChecked ->
-            // Handle toggle logic here
+//            btnDay.setBackgroundColor(colorOn)
+//            btnWeek.setBackgroundColor(Color.Blue)
+//            btnMonth.setBackgroundColor(Color.Blue)
+//            btnYear.setBackgroundColor(Color.Blue)
         }
 
         btnCancel.setOnClickListener {
@@ -46,3 +60,4 @@ class RepeatSettingsDialogFragment : DialogFragment() {
         )
     }
 }
+
