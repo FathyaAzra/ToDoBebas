4b459b9aec8e476d194e91f84df4fdeeb20f5853
Abiyan Agitia Tarigan <abiyanagitia12@gmail.com>
2024-12-09 13:47:13 +0700

    Dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -83,6 +83,8 @@ dependencies {
     kapt(libs.androidx.room.compiler)
     implementation(libs.androidx.room.ktx)
     implementation ("com.github.PhilJay:MPAndroidChart:v3.1.0")
+    implementation ("androidx.room:room-runtime:2.5.2")
+    kapt ("androidx.room:room-compiler:2.5.2")
 
 
 
